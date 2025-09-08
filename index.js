4be809219fe322ad20dfce65124aa0c7f2c1c48b
Veliz <paucarcarlos2108@gmail.com>
2025-09-07 22:41:24 -0500

    feat: crear menu para teléfonos

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const btnToggleMode = document.getElementById('btn-toggle-mode');
-const btnLanguageSwitcher = document.getElementById('language-switcher');
+const btnsToggleMode = document.querySelectorAll('.btn-toggle-mode');
+const btnsLanguageSwitcher = document.querySelectorAll('.language-switcher');
 const DEFAULT_LANG = 'es';
+const btnOpenMenu = document.getElementById('btn-open-menu');
+let menuOpen = false;
+const overlay = document.getElementById('overlay');
+const menuMobile = document.getElementById('menu-mobile');
 
 function isDarkMode (){
     const mode = localStorage.getItem('mode');
@@ -10,28 +14,30 @@ function isDarkMode (){
 
 function setMode(mode){
     localStorage.setItem('mode', mode);
-    const sunIcon = document.getElementById('sun-icon');
-    const moonIcon = document.getElementById('moon-icon');
+    const sunIcons = document.querySelectorAll('.sun-icon');
+    const moonIcons = document.querySelectorAll('.moon-icon');
     if(mode){
         if(mode === 'dark'){
             document.documentElement.setAttribute('data-theme', 'dark');
             localStorage.setItem('mode', 'dark');
-            sunIcon.style.display = 'block';
-            moonIcon.style.display = 'none';
+            sunIcons.forEach(icon => icon.style.display = 'block');
+            moonIcons.forEach(icon => icon.style.display = 'none');
         }else{
             document.documentElement.setAttribute('data-theme', 'light');
             localStorage.setItem('mode', 'light');
-            sunIcon.style.display = 'none';
-            moonIcon.style.display = 'block';
+            sunIcons.forEach(icon => icon.style.display = 'none');
+            moonIcons.forEach(icon => icon.style.display = 'block');
         }
     }
 }
 
-if(btnToggleMode){
-    btnToggleMode.addEventListener('click', () => {
-        const mode = isDarkMode() ? 'light' : 'dark';
-        setMode(mode);
-    });
+if(btnsToggleMode){
+    btnsToggleMode.forEach(btnToggleMode => {
+        btnToggleMode.addEventListener('click', () => {
+            const mode = isDarkMode() ? 'light' : 'dark';
+            setMode(mode);
+        });
+    })
 }
 
 async function loadLanguage(lang = 'es'){
@@ -49,12 +55,31 @@ async function loadLanguage(lang = 'es'){
     }
 }
 
-if(btnLanguageSwitcher){
-    btnLanguageSwitcher.addEventListener('change', (e) => {
-        const LANG = e.target.value;
-        loadLanguage(LANG);
-        localStorage.setItem('lang', LANG);
+if(btnsLanguageSwitcher){
+    btnsLanguageSwitcher.forEach(btnLanguageSwitcher => {
+        btnLanguageSwitcher.addEventListener('change', (e) => {
+            const LANG = e.target.value;
+            loadLanguage(LANG);
+            localStorage.setItem('lang', LANG);
+        })
     })
+}
+
+if(overlay){
+    overlay.addEventListener('click', () =>{
+        menuMobile.style.display = menuOpen? 'none' : 'flex';
+        menuOpen = false;
+        overlay.style.display = 'none';
+    })
+}
+
+if(btnOpenMenu){
+    btnOpenMenu.addEventListener('click', () =>{
+        menuOpen = !menuOpen;
+        menuOpen? overlay.style.display = 'block' : overlay.style.display = 'none';
+        menuMobile.style.display = menuOpen ? 'flex' : 'none';
+        console.log(menuOpen);
+    });
 }
 
 function initMode(){
@@ -65,10 +90,13 @@ function initMode(){
 function initLang(){
     const lang = localStorage.getItem('lang') || DEFAULT_LANG;
     loadLanguage(lang);
-    if(btnLanguageSwitcher){
-        btnLanguageSwitcher.value = lang;
+    if(btnsLanguageSwitcher){
+        btnsLanguageSwitcher.forEach(btnLanguageSwitcher => {
+            btnLanguageSwitcher.value = lang;
+        })
     }
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     initMode();
